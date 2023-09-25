b7460688f5f5f29152143e6ab562eaf0bd39488d
Serhii <serhii.furkalo@keyua.org>
2023-09-25 17:48:57 +0300

    add templates to project

--- todos_service/app/database.py
+++ todos_service/app/database.py
@@ -3,10 +3,11 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-environ.Env.read_env()
-env = environ.Env()
-SQLALCHEMY_DATABASE_URL = f"postgresql://{env('POSTGRES_USER')}:{env('POSTGRES_PASSWORD')}@{env('DB_IP')}/{env('POSTGRES_DB')}"
+# environ.Env.read_env()
+# env = environ.Env()
 
+# SQLALCHEMY_DATABASE_URL = f"postgresql://{env('POSTGRES_USER')}:{env('POSTGRES_PASSWORD')}@{env('DB_IP')}/{env('POSTGRES_DB')}"
+SQLALCHEMY_DATABASE_URL = 'sqlite:///./todosapp.db'
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessoinLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
--- todos_service/app/routers/todos.py
+++ todos_service/app/routers/todos.py
@@ -1,5 +1,9 @@
-from fastapi import APIRouter, Request
+from typing import Annotated
+from fastapi import APIRouter, Depends, Request
 from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
+from app.database import SessoinLocal
+from sqlalchemy.orm import Session
 
 
 router = APIRouter(
@@ -8,8 +12,28 @@ router = APIRouter(
     responses={404: {'description': 'Not found'}}
 )
 
-templates = Jinja2Templates(directory='app/templates')
+def get_db():
+    db = SessoinLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
-@router.get('/test')
-async def test(request: Request):
+
+templates = Jinja2Templates(directory='app/templates')
+db_dependency = Annotated[Session, Depends(get_db)]
+
+
+@router.get('/', response_class=HTMLResponse)
+async def read_all_by_user(request: Request):
     return templates.TemplateResponse('home.html', {'request': request})
+
+
+@router.get('/add-todo', response_class=HTMLResponse)
+async def add_new_todo(request: Request):
+    return templates.TemplateResponse('add-todo.html', {'request': request})
+
+
+@router.get('/edit-todo/{todo_id}', response_class=HTMLResponse)
+async def edit_todo(request: Request):
+    return templates.TemplateResponse('edit-todo.html', {'request': request})
